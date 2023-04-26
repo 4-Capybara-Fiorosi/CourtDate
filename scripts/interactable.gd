3677bd7abf319dec87b161488841f5f756dfd112
Al-Andrew <aldea.andrei.977@gmail.com>
2023-04-26 20:20:47 +0300

    [improve] Made base interactable better
    We now assign the material at runtime so you can ommit it from the sprite settings.
    Also made the shader logic local to each instance.

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -1,16 +1,23 @@
 class_name Interactable
 extends Area2D
 
-@onready var skin := get_child(0)
+@export var active_color : Color = Color.YELLOW;
+@export var skin_path : NodePath;
+@onready var skin :Sprite2D = get_node(skin_path);
 
+@onready var interactable_material :Material = preload("res://res/shaders/interactable_material.tres");
+
+func _ready() -> void:
+	skin.material = interactable_material.duplicate()
+	skin.material.set("shader_parameter/outline_color", active_color);
 
 func player_entered():
-	(skin as Sprite2D).material.set("shader_parameter/width", 1)
+	skin.material.set("shader_parameter/width", 1);
 
 
 func player_exit():
-	(skin as Sprite2D).material.set("shader_parameter/width", 0)
+	skin.material.set("shader_parameter/width", 0);
 
 
-func interact(player: PlayerCharacter):
+func interact(_player: PlayerCharacter):
 	pass
