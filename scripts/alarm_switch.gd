f7c791b50549ef1a87157517f58f85ceabac84eb
DELL125\Rares Opariuc <opariucraresioan@gmail.com>
2023-05-03 22:42:33 +0300

    added the light scene

---
+++ scripts/alarm_switch.gd
@@ -0,0 +1,13 @@
+extends Interactable
+
+@export var door_to_open: NodePath
+
+var closedSwitch = preload("res://res/tiles/LVL3/alarn_switch_closed.png")
+var openedSwitch = preload("res://res/tiles/LVL3/alarm_switch_opened.png")
+
+@onready var door: Door = get_node(door_to_open)
+
+func interact(_player: PlayerCharacter):
+	door.open()
+	$Skin.texture = closedSwitch
+	$Shape.visible = false
